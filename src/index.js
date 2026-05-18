beb9439fec3c10c23a9c31eaa53da1d3259d2af7
Andriy Sydorchuk <programmers130822@gmail.com>
2026-05-18 20:07:01 +0300

    Rename appController to appManager

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './style.css';
-import { AppController } from './appController';
+import { AppManager } from './appManager';
 
 let userAction;
 do {
-    userAction = AppController.getUserAction();
-    AppController.handleUserAction(userAction);
+    userAction = AppManager.getUserAction();
+    AppManager.handleUserAction(userAction);
 } while (userAction !== "exit");

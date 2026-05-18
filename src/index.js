9440fd23a2194006987c6de1a1703d0e5d95a6c5
Andriy Sydorchuk <programmers130822@gmail.com>
2026-05-18 14:25:33 +0300

    Implement basic app setup

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import './style.css';
+import { AppController } from './appController';
 
+let userAction;
+do {
+    userAction = AppController.getUserAction();
+    AppController.handleUserAction(userAction);
+} while (userAction !== "exit");

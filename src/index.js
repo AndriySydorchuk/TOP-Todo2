663385e599a9d9625bcc8a5125da4e3c500d6ee3
Andriy Sydorchuk <programmers130822@gmail.com>
2026-05-20 13:20:27 +0300

    Comment previous console logic and and domManager initialization

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import './style.css';
 import { AppManager } from './appManager';
 import { collectionManager } from './collectionManager';
+import { domManager } from './domManager';
 
 AppManager.init();
 collectionManager.init();
+domManager.init();
 
-let userAction;
-do {
-    userAction = AppManager.getUserAction();
-    AppManager.handleUserAction(userAction);
-} while (userAction !== "exit");
+// let userAction;
+// do {
+//     userAction = AppManager.getUserAction();
+//     AppManager.handleUserAction(userAction);
+// } while (userAction !== "exit");

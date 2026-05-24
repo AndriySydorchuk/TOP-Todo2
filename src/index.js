be6976b5bcf6edd620573645965bcc3e5b485c87
Andriy Sydorchuk <programmers130822@gmail.com>
2026-05-24 20:50:26 +0300

    Remove commented section containing console vesrion code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import './style.css';
-import { AppManager } from './appManager';
+import { appManager } from './appManager';
 import { collectionManager } from './collectionManager';
 import { domManager } from './domManager';
 
-AppManager.init();
+appManager.init();
 collectionManager.init();
 domManager.init();
-
-// let userAction;
-// do {
-//     userAction = AppManager.getUserAction();
-//     AppManager.handleUserAction(userAction);
-// } while (userAction !== "exit");
